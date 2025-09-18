d963eb8f81bd4e08afed78b3c8269a617b4f65d1
ANTU SHIL <anturoy084@gmail.com>
2025-09-18 20:02:20 +0600

    added react conceptual sessions

---
+++ src/Components/User.jsx
@@ -0,0 +1,7 @@
+export default function User() {
+    return (
+        <div>
+            <h2>Lorem ipsum dolor ant.</h2>
+        </div>
+    )
+}
---
+++ src/Components/Users.jsx
@@ -0,0 +1,8 @@
+export default function Users({person}){
+    return(
+        <div>
+            <h1>Hello duniya{person.name}</h1>
+        </div>
+
+    )
+}
